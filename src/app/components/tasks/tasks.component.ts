d1c6653a3c535c0817fad34fe9d4b6d7735d245b
constanzaGiacoboni <constanza.ibarra@est.fi.uncoma.edu.ar>
2022-03-23 09:18:17 -0300

    delete and services

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { TaskService } from 'src/app/services/task.service';
 import { Task } from 'src/app/Task';
-import {TASKS} from '../../mock_tasks';
 
 @Component({
   selector: 'app-tasks',
@@ -8,11 +8,19 @@ import {TASKS} from '../../mock_tasks';
   styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent implements OnInit {
-  tasks: Task[] = TASKS;
+  tasks: Task[] = [];
 
-  constructor() { }
+  constructor(private taskService: TaskService) { }
 
   ngOnInit(): void {
+    this.taskService.getTasks().subscribe( (tasks) => (this.tasks = tasks) );
+  }
+
+  deleteTask(task : Task){
+    this.taskService
+      .deleteTask(task)
+      .subscribe( 
+        () => (this.tasks = this.tasks.filter( (t) => t.id !== task.id)) );
   }
 
 }
